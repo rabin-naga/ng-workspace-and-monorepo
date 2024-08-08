bbee4c57aaaff459da4dcd8ef3a3f699ed187c30
Rabin Naga <rabin.naga@fonenxt.com>
2024-08-08 13:47:04 +0545

    Add home module

--- projects/app-one/src/app/app.routes.ts
+++ projects/app-one/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeModule } from './home/home.module';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./home/home.module').then((module) => HomeModule),
+  },
+];
