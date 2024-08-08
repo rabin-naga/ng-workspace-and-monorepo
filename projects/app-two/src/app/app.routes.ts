321d1af3d158d090f9e5ecce915e94d878995550
Rabin Naga <rabin.naga@fonenxt.com>
2024-08-08 14:11:36 +0545

    Use app-one module in app-two

--- projects/app-two/src/app/app.routes.ts
+++ projects/app-two/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeModule } from '../../../app-one/src/app/home/home.module';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./app.component').then((module) => HomeModule),
+  },
+];
