a847bda28b547cedf8737585a7e503cab95b58a6
Rabin Naga <rabin.naga@fonenxt.com>
2024-08-11 14:14:26 +0545

    Merge app-two dev to main

--- projects/app-two/src/app/app.routes.ts
+++ projects/app-two/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeModule } from '../../../app-one/src/app/home/home.module';
+import { AboutComponent } from '../../../app-one/src/app/about/about.component';
 
 export const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./app.component').then((module) => HomeModule),
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
   },
 ];
